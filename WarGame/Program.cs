5caebc3f3534ac351ef826babe4b6285f26c80a3
Brian Lai <brianlai30@outlook.com>
2021-05-27 10:30:50 -0700

    Created project

---
+++ WarGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WarGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

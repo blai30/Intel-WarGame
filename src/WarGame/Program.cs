41a02e28fba082b217664893f472a9a1624933b8
Brian Lai <brianlai30@outlook.com>
2021-05-27 20:13:06 -0700

    feat: set up Program to run game

---
+++ src/WarGame/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using WarGame;
+
+Console.WriteLine(DateTime.UtcNow.ToString("R"));
+Console.WriteLine(Environment.ProcessId);
+
+// Initialize random object with seed generated from GUID.
+Random random = new Random(new Guid().GetHashCode());
+
+Game game = new Game(2);
+game.Start(random);

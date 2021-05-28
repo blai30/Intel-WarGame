db5df67f6eabbac5b28d176a65a6dda592704b8b
Brian Lai <brianlai30@outlook.com>
2021-05-27 20:12:54 -0700

    feat: create Dealer and Game classes

---
+++ src/WarGame/Dealer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WarGame
+{
+    /// <summary>
+    ///     Represents a collection of cards with the ability to shuffle.
+    /// </summary>
+    public class Dealer
+    {
+        private readonly Random _random;
+
+        public Queue<Card> Cards { get; private set; }
+
+        public Dealer(Random random)
+        {
+            _random = random;
+        }
+
+        public void Initialize()
+        {
+            List<Card> cards = new List<Card>(52);
+
+            foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+            {
+                foreach (Rank rank in Enum.GetValues(typeof(Rank)))
+                {
+                    Card card = new Card(suit, rank);
+                    cards.Add(card);
+                }
+            }
+
+            Cards = Shuffle(cards);
+        }
+
+        private Queue<Card> Shuffle(IList<Card> list)
+        {
+            for (int i = list.Count - 1; i > 0; i--)
+            {
+                int j = _random.Next(0, i + 1);
+                Card temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+            }
+
+            Queue<Card> shuffled = new Queue<Card>(list);
+            return shuffled;
+        }
+    }
+}
---
+++ src/WarGame/Game.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WarGame
+{
+    public class Game
+    {
+        private List<Player> _players;
+
+        /// <summary>
+        ///     Create a new game and define the number of players.
+        /// </summary>
+        /// <param name="playerCount">Number of players participating.</param>
+        public Game(int playerCount = 2)
+        {
+            _players = new List<Player>(playerCount);
+            for (int i = 0; i < playerCount; i++)
+            {
+                var player = new Player();
+                _players.Add(player);
+            }
+        }
+
+        public void Start(Random random)
+        {
+            Console.WriteLine("Starting game");
+
+            Dealer dealer = new Dealer(random);
+            dealer.Initialize();
+
+            int initialDeckSize = 52 / _players.Count;
+            foreach (Player player in _players)
+            {
+                for (int i = 0; i < initialDeckSize; i++)
+                {
+                    player.Deck.Enqueue(dealer.Cards.Dequeue());
+                }
+            }
+
+            Console.WriteLine("Dealt");
+        }
+    }
+}

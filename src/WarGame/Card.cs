3444177a94c78698b95ebc7c03db2864c43d8fa6
Brian Lai <brianlai30@outlook.com>
2021-05-27 20:12:23 -0700

    feat: create Card and Player classes

---
+++ src/WarGame/Card.cs
@@ -0,0 +1,34 @@
+﻿namespace WarGame
+{
+    public enum Suit
+    {
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades,
+    }
+
+    public enum Rank
+    {
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13,
+        Ace = 14,
+    }
+
+    /// <summary>
+    ///     A single card entity identified by suit and rank.
+    /// </summary>
+    /// <param name="Suit"></param>
+    /// <param name="Rank"></param>
+    public record Card(Suit Suit, Rank Rank);
+}
---
+++ src/WarGame/Player.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace WarGame
+{
+    public class Player
+    {
+        public Queue<Card> Deck = new(52);
+    }
+}

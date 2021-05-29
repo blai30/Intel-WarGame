3548f08e14207acd888810b6a6d5307b51e6e428
Brian Lai <brianlai30@outlook.com>
2021-05-28 18:49:24 -0700

    refactor: add getter to Player Deck property

--- src/WarGame/Player.cs
+++ src/WarGame/Player.cs
@@ -5,7 +5,7 @@ namespace WarGame
     public class Player
     {
         public string Name { get; }
-        public Queue<Card> Deck = new();
+        public Queue<Card> Deck { get; } = new();
 
         public Player(string name)
         {

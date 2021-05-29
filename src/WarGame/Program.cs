19035abfcef5aee44e4fc8dd2021f7d2581ac9e3
Brian Lai <brianlai30@outlook.com>
2021-05-28 20:01:05 -0700

    refactor: remove timestamp and PID from program start

--- src/WarGame/Program.cs
+++ src/WarGame/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using WarGame;
 
-Console.WriteLine(DateTime.UtcNow.ToString("R"));
-Console.WriteLine(Environment.ProcessId);
-
 // Initialize random object with seed generated from GUID.
 Random random = new Random(Guid.NewGuid().GetHashCode());
 

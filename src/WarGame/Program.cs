f47dc1785d4eba5cea7ce19f71f0314bbca56a7a
Brian Lai <brianlai30@outlook.com>
2021-10-18 16:46:30 -0700

    fix: names no longer show up empty when entering no name

--- src/WarGame/Program.cs
+++ src/WarGame/Program.cs
@@ -5,10 +5,12 @@ using WarGame.Core;
 Random random = new Random(Guid.NewGuid().GetHashCode());
 
 Console.Write("Enter name for player 1: ");
-string name1 = Console.ReadLine() ?? "Player 1";
+string name1 = Console.ReadLine();
+name1 = string.IsNullOrEmpty(name1) ? "Player 1" : name1;
 
 Console.Write("Enter name for player 2: ");
-string name2 = Console.ReadLine() ?? "Player 2";
+string name2 = Console.ReadLine();
+name2 = string.IsNullOrEmpty(name2) ? "Player 2" : name2;
 
 Game game = new Game(random, name1, name2);
 game.Start();
